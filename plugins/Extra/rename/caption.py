9bfde93c9a09e3f50fde0ae6068160ed87d08734
NeonAn <justasneon@gmail.com>
2025-07-23 04:13:41 +0530

    Update caption.py

--- plugins/Extra/rename/caption.py
+++ plugins/Extra/rename/caption.py
@@ -7,9 +7,9 @@ from info import RENAME_MODE
 async def add_caption(client, message):
     if RENAME_MODE == False:
         return 
-    caption = await client.ask(message.chat.id, "**__Give me a Caption to Set__ 😇**\n\n**__Available Filling__ 🧭** :-\n**📂 __File Name__**: `{filename}`\n\n**💾 __Size__**: `{filesize}`\n\n**⏰ __Duration__**: `{duration}`**")
+    caption = await client.ask(message.chat.id, "**__Give me a Caption to Set__ 😇**\n\n**__Available Filling__ 🧭** :-\n\n**📂 __File Name__**: `{filename}`\n\n**💾 __Size__**: `{filesize}`\n\n**⏰ __Duration__**: `{duration}`**")
     await db.set_caption(message.from_user.id, caption=caption.text)
-    await message.reply_text("__**Your Caption is Successfully Saved ✅**__")
+    await message.reply_text("__**Your Caption Successfully Saved ✅**__")
 
     
 @Client.on_message(filters.private & filters.command('del_caption'))
@@ -18,9 +18,9 @@ async def delete_caption(client, message):
         return 
     caption = await db.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("🥲 **__Sorry !! No Caption found...__** 🥹")
+       return await message.reply_text("🥲 **__Sorry !! No Caption Found...__** 🥹")
     await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**__Your Caption is Successfully Deleted__** ❌")
+    await message.reply_text("**__Your Caption Successfully Deleted__** ❌")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message):
@@ -30,4 +30,4 @@ async def see_caption(client, message):
     if caption:
        await message.reply_text(f"**Your Caption:-**\n\n`{caption}`")
     else:
-       await message.reply_text("🥲 **__Sorry !! No Caption found...__** 🥹")
+       await message.reply_text("🥲 **__Sorry !! No Caption Found...__** 🥹")

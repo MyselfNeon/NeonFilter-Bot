31168c9c8bf367b2031ef9058c4715ff8d72059e
NeonAn <justasneon@gmail.com>
2025-07-22 18:34:11 +0530

    Update rename.py

--- plugins/Extra/rename/rename.py
+++ plugins/Extra/rename/rename.py
@@ -19,7 +19,7 @@ async def rename_start(client, message):
         filename = file.file_name
         filesize = humanize.naturalsize(file.file_size) 
         fileid = file.file_id
-        text = f"""<blockquote>**‣ 𝗡𝗼𝘄 𝗘𝗻𝘁𝗲𝗿 𝗡𝗲𝘄 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 ....**</blockquote>\n\n<blockquote>**__Original File Name__** :</blockquote>\n`{filename}`\n\n**__Original File Size__** : `{filesize}`"""
+        text = f"""<blockquote>**‣ 𝗡𝗼𝘄 𝗘𝗻𝘁𝗲𝗿 𝗡𝗲𝘄 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 ....**</blockquote>\n\n<blockquote>**__‣ Original File Name__** :</blockquote>\n`{filename}`\n\n**__Original File Size__** : `{filesize}`"""
         await message.reply_text(text)
         kk = await client.listen(message.from_user.id)
         await refunc(client, message, kk.text, msg)

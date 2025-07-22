5c7cf058c9154240203388db1f795b1a6e7e3c81
NeonAn <justasneon@gmail.com>
2025-07-22 18:33:30 +0530

    Update rename.py

--- plugins/Extra/rename/rename.py
+++ plugins/Extra/rename/rename.py
@@ -11,7 +11,7 @@ import random
 async def rename_start(client, message):
     if RENAME_MODE == False:
         return 
-    msg = await client.ask(message.chat.id, "**__Now send me your File/Video/Audio to Rename 📝__**")
+    msg = await client.ask(message.chat.id, "**__Now send me your File/Video/Audio you want to Rename 📝__**")
     if not msg.media:
         return await message.reply("**__Please send me supported media.__**")
     if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT, enums.MessageMediaType.AUDIO]:
@@ -19,7 +19,7 @@ async def rename_start(client, message):
         filename = file.file_name
         filesize = humanize.naturalsize(file.file_size) 
         fileid = file.file_id
-        text = f"""<blockquote>**𝗡𝗼𝘄 𝗘𝗻𝘁𝗲𝗿 𝗡𝗲𝘄 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 ....**</blockquote>\n\n**__Original File Name__** :\n`{filename}`\n\n**__Original File Size__** : `{filesize}`"""
+        text = f"""<blockquote>**‣ 𝗡𝗼𝘄 𝗘𝗻𝘁𝗲𝗿 𝗡𝗲𝘄 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 ....**</blockquote>\n\n<blockquote>**__Original File Name__** :</blockquote>\n`{filename}`\n\n**__Original File Size__** : `{filesize}`"""
         await message.reply_text(text)
         kk = await client.listen(message.from_user.id)
         await refunc(client, message, kk.text, msg)

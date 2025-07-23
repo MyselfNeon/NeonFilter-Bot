38841a0533bb601ec03be7cd7f4585e5c6becf92
NeonAn <justasneon@gmail.com>
2025-07-23 15:55:42 +0530

    Update telegraph.py

--- plugins/Extra/telegraph.py
+++ plugins/Extra/telegraph.py
@@ -24,26 +24,26 @@ def upload_image_requests(image_path):
 
 @Client.on_message(filters.command("telegraph") & filters.private)
 async def telegraph_upload(bot, update):
-    t_msg = await bot.ask(chat_id = update.from_user.id, text = "Now Send Me Your Photo Or Video Under 5MB To Get Media Link.")
+    t_msg = await bot.ask(chat_id = update.from_user.id, text = "**__Now Send Me Your Photo Or Video Under 5MB To Get Media Link 🖇️__**")
     if not t_msg.media:
-        return await update.reply_text("**Only Media Supported.**")
+        return await update.reply_text("**__Only Media Supported 😄__**")
     path = await t_msg.download()
-    uploading_message = await update.reply_text("<b>ᴜᴘʟᴏᴀᴅɪɴɢ...</b>")
+    uploading_message = await update.reply_text("<b><i>Uploading now 📤 ...</i></b>")
     try:
         image_url = upload_image_requests(path)
         if not image_url:
-            return await uploading_message.edit_text("**Failed to upload file.**")
+            return await uploading_message.edit_text("**__Failed to upload file 💢__**")
     except Exception as error:
-        await uploading_message.edit_text(f"**Upload failed: {error}**")
+        await uploading_message.edit_text(f"**__Upload failed: {error}__**")
         return
     await uploading_message.edit_text(
-        text=f"<b>Link :-</b>\n\n<code>{image_url}</code>",
+        text=f"<b><blockquote>‣ 𝐘𝐎𝐔𝐑 𝐋𝐈𝐍𝐊 </b></blockquote>\n\n<code>{image_url}</code>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton(text="Open Link", url=image_url),
-            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url={image_url}")
+            InlineKeyboardButton(text="Oᴘᴇɴ Lɪɴᴋ 🔓", url=image_url),
+            InlineKeyboardButton(text="Sʜᴀʀᴇ Lɪɴᴋ 🖇️", url=f"https://telegram.me/share/url?url={image_url}")
             ],[
-            InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
+            InlineKeyboardButton(text="❌ Cʟᴏsᴇ ❌", callback_data="close")
             ]])
         )
     

ef3539419317ecd9bfc85942cd50f58b72024b37
NeonAn <justasneon@gmail.com>
2025-07-23 08:40:41 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -11,9 +11,9 @@ import random
 async def stream_start(client, message):
     if STREAM_MODE == False:
         return 
-    msg = await client.ask(message.chat.id, "**Now send me your file/video to get stream and download link**")
+    msg = await client.ask(message.chat.id, "**__Now send me your file/video to get stream and Download link__**")
     if not msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
-        return await message.reply("**Please send me supported media.**")
+        return await message.reply("**__Please send me Supported Media.__**")
     if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
         file = getattr(msg, msg.media.value)
         filename = file.file_name
@@ -31,22 +31,22 @@ async def stream_start(client, message):
         download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
  
         await log_msg.reply_text(
-            text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+            text=f"•• Link generated for ID #{user_id} \n•• Username : {username} \n\n•• File Name : {fileName}",
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[
                     InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # web download Link
-                    InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)   # web stream Link
+                    InlineKeyboardButton('🖥️ Watch Online 🖥️', url=stream)   # web stream Link
                 ]]
             )
         )
         rm=InlineKeyboardMarkup(
             [[
-                InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=download)
+                InlineKeyboardButton("Sᴛʀᴇᴀᴍ 🖥", url=stream),
+                InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ 📥', url=download)
             ]] 
         )
-        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
+        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ Sɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥 Wᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Note : Link won't Expire till i Delete</b>"""
 
         await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)

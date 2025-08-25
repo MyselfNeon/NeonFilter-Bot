05056084bfe7df0ea3646a1e9336c1f565067736
NeonAn <justasneon@gmail.com>
2025-08-25 14:55:29 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -43,6 +43,8 @@ async def git(bot, message):
             txt += f'\n<b><i>Lᴀɴɢᴜᴀɢᴇ : {qw.get("language")}</i></b>'
         if qw.get("created_at"):
             txt += f'\n<b><i>Cʀᴇᴀᴛᴇᴅ Oɴ : {qw.get("created_at").replace("Z", "").replace("T", " T")}</i></b>'            
+        if qw.get("description"):
+            txt += f'\n\n<b><i>📝 Dᴇsᴄʀɪᴘᴛɪᴏɴ :</b></i>\n<blockquote expandable>{qw.get("description")}</blockquote>'
 
         if qw.get("archived") == True:
             txt += f"<b><i>\nTʜɪs Pʀᴏjᴇᴄᴛ Is Aʀᴄʜɪᴠᴇᴅ 🔐</i></b>"       

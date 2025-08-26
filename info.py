f374b6628d9c6112a02419318ae60531103eea76
NeonAn <justasneon@gmail.com>
2025-08-27 00:05:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) #
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '5')) # number of referal count
 REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1month') # time in week, day, month.
 PAYMENT_QR = environ.get('PAYMENT_QR', 'https://telegra.ph/file/a5b4baeeb15236bd37305-31cacf719a14a78183.jpg') # payment code picture url.
-PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b><blockquote>‣ 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 𝐏𝐋𝐀𝐍𝐒 📝</blockquote>\n<i>• 30Rs - 01 Week\n• 50Rs - 01 Month\n• 120Rs - 03 Months\n• 220Rs - 06 Months</i>\n\n<blockquote>‣ 𝐏𝐋𝐀𝐍 𝐁𝐄𝐍𝐄𝐅𝐈𝐓𝐒 ✨</blockquote>\n<i>• No Need To Verify\n• No Need To Open Links\n• Direct Files\n• Ad-Free Experience\n• High Speed Download\n• Multiplayer Streaming Links\n• Unlimited Movies, Animes & Series\n• 24×7 Admin Support\n• Requests Will Be Completed Within 01 Hour Of Submission If Available</i>\n\n<blockquote>‣ 𝐔𝐏𝐈 𝐈𝐃 🆔</blockquote> - <code>neonan23@ibl</code>\n\n<i>📌 Click /myplan To Check Your Plan\n• Send Screenshots After Payment\n• After Sending Screenshot Give Us Some Time To Add You In Premium</i></b>')
+PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b><blockquote>‣ 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 𝐏𝐋𝐀𝐍𝐒 📝</blockquote>\n<i>• 30Rs - 01 Week\n• 50Rs - 01 Month\n• 120Rs - 03 Months\n• 220Rs - 06 Months</i>\n\n<blockquote>‣ 𝐏𝐋𝐀𝐍 𝐁𝐄𝐍𝐄𝐅𝐈𝐓𝐒 ✨</blockquote>\n<i>• No Need To Verify\n• No Need To Open Links\n• Direct Files\n• Ad-Free Experience\n• High Speed Download\n• Multiplayer Streaming Links\n• Unlimited Movies, Animes & Series\n• 24×7 Admin Support\n• Requests Will Be Completed Within 01 Hour Of Submission If Available</i>\n\n<blockquote>‣ 𝐔𝐏𝐈 𝐈𝐃 🆔</blockquote> - <code>neonan23@ibl</code>\n\n<i>• Click /myplan To Check Your Plan\n• Send Screenshots After Payment\n• After Sending Screenshot Give Us Some Time To Add You In Premium</i></b>')
 
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
@@ -182,6 +182,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 

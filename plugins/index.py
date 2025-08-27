718c5ea055794446bd77b9d824f5228a4859eb5c
NeonAn <justasneon@gmail.com>
2025-08-27 22:32:18 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -93,7 +93,7 @@ async def send_for_index(bot, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         return await message.reply(
-            f'**__Do you Want To Index This Channel or Group ?\n\n🆔 Chat ID/ Username :__ ⬇️** \n<code>{chat_id}</code>\n\n**📄 __Last Message ID :__ ⬇️** \n<code>{last_msg_id}</code>',
+            f'**__Do you Want To Index This Channel or Group ?\n\n🆔 Chat ID/ Username :__** \n<code>▶️ {chat_id} ◀️</code>\n\n**📄 __Last Message ID :__ ** <code>{last_msg_id}</code>',
             reply_markup=reply_markup
         )
 
@@ -186,6 +186,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             await k.reply_text("**__If You Get Message Not Modified Error Then Skip Your Saved File Then Index Again__**")
         else:
             await msg.edit(f'**__Sᴜᴄᴄᴇssғᴜʟʟʏ Sᴀᴠᴇᴅ__ ✅ : <code>{total_files}</code> __To DataBase!\nDᴜᴘʟɪᴄᴀᴛᴇ Fɪʟᴇs Sᴋɪᴘᴘᴇᴅ :__ <code>{duplicate}</code>\n__Dᴇʟᴇᴛᴇᴅ Msɢs Sᴋɪᴘᴘᴇᴅ :__ <code>{deleted}</code>\n__Nᴏɴ-Mᴇᴅɪᴀ Msɢs Sᴋɪᴘᴘᴇᴅ :__ <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\n__Eʀʀᴏʀs Oᴄᴄᴜʀʀᴇᴅ__ : <code>{errors}</code>**')
+
 
 
 

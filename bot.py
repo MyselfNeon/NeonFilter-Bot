33c892546fb45d2743ab28ba2b833cb7017141f7
NeonAn <justasneon@gmail.com>
2025-08-28 18:49:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ async def start():
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
         sys.modules[import_path] = mod
-        print(f"MyselfNeon Imported => {plugin_name}")
+        print(f"✨ Neon Imported => {plugin_name}")
     # -------------------------------------------------------
 
     if ON_HEROKU:
@@ -104,4 +104,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
         
+
 

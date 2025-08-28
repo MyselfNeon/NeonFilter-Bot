84798463b4a127e26e691dc6a7ddc3c2c3dfa6f0
NeonAn <justasneon@gmail.com>
2025-08-28 06:58:50 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -1,52 +1,72 @@
 from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import STREAM_MODE, URL, LOG_CHANNEL
 from urllib.parse import quote_plus
 from Neon.util.file_properties import get_name, get_hash, get_media_file_size
 from Neon.util.human_readable import humanbytes
-import humanize
 import random
 
 @Client.on_message(filters.private & filters.command("stream"))
 async def stream_start(client, message):
-    if STREAM_MODE == False:
+    if STREAM_MODE is False:
         return 
-    msg = await client.ask(message.chat.id, "**__Now send me your file/video to get stream and Download link__**")
-    if not msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+    
+    msg = await client.ask(
+        message.chat.id, 
+        "**__Now send me your file/video to get stream and Download link__**"
+    )
+
+    # only accept video or document
+    if msg.media not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
         return await message.reply("**__Please send me Supported Media.__**")
-    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
-        file = getattr(msg, msg.media.value)
-        filename = file.file_name
-        filesize = humanize.naturalsize(file.file_size) 
-        fileid = file.file_id
-        user_id = message.from_user.id
-        username =  message.from_user.mention 
+    
+    file = getattr(msg, msg.media)
+    filename = file.file_name
+    filesize = humanbytes(get_media_file_size(msg))  # consistent usage
+    fileid = file.file_id
+    user_id = message.from_user.id
+    username = message.from_user.mention 
 
-        log_msg = await client.send_cached_media(
-            chat_id=LOG_CHANNEL,
-            file_id=fileid,
-        )
-        fileName = {quote_plus(get_name(log_msg))}
-        stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+    # forward to log channel
+    log_msg = await client.send_cached_media(
+        chat_id=LOG_CHANNEL,
+        file_id=fileid,
+    )
+
+    # file name (fixed: no { })
+    fileName = quote_plus(get_name(log_msg))
+
+    # links
+    stream = f"{URL}watch/{str(log_msg.id)}/{fileName}?hash={get_hash(log_msg)}"
+    download = f"{URL}{str(log_msg.id)}/{fileName}?hash={get_hash(log_msg)}"
  
-        await log_msg.reply_text(
-            text=f"•• Link generated for ID #{user_id} \n•• Username : {username} \n\n•• File Name : {fileName}",
-            quote=True,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [[
-                    InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # web download Link
-                    InlineKeyboardButton('🖥️ Watch Online 🖥️', url=stream)   # web stream Link
-                ]]
-            )
-        )
-        rm=InlineKeyboardMarkup(
+    # log channel info
+    await log_msg.reply_text(
+        text=f"•• Link generated for ID #{user_id} \n•• Username : {username} \n\n•• File Name : {get_name(log_msg)}",
+        quote=True,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
             [[
-                InlineKeyboardButton("Sᴛʀᴇᴀᴍ 🖥", url=stream),
-                InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ 📥', url=download)
-            ]] 
+                InlineKeyboardButton("🚀 Fast Download 🚀", url=download),
+                InlineKeyboardButton('🖥️ Watch Online 🖥️', url=stream)
+            ]]
         )
-        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ Sɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥 Wᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Note : Link won't Expire till i Delete</b>"""
+    )
 
-        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
+    # user reply buttons
+    rm = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("Sᴛʀᴇᴀᴍ 🖥", url=stream),
+            InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ 📥', url=download)
+        ]] 
+    )
+
+    # final message to user
+    msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ Sɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b>🖥 Wᴀᴛᴄʜ :</b> <i>{}</i>\n\n<b>🚸 Note : Link won't Expire till i Delete</b>"""
+
+    await message.reply_text(
+        text=msg_text.format(get_name(log_msg), filesize, download, stream),
+        quote=True,
+        disable_web_page_preview=True,
+        reply_markup=rm
+    )

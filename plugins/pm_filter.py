277502d5444c3fef87a2009b36717591feac6c24
NeonAn <justasneon@gmail.com>
2025-09-02 01:59:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2221,7 +2221,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(https://files.catbox.moe/p0qhxt.jpg)
+            InputMediaPhoto("https://files.catbox.moe/p0qhxt.jpg")
         )
         reply_markup = InlineKeyboardMarkup(btn)
         await query.message.edit_text(
@@ -3288,4 +3288,5 @@ async def global_filters(client, message, text=False):
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 

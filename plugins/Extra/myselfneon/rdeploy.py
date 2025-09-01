95a37fe4bc9bbc5834bf5bb2e3c39a748dbe7bcb
NeonAn <justasneon@gmail.com>
2025-09-01 19:29:38 +0530

    Update rdeploy.py

--- plugins/Extra/myselfneon/rdeploy.py
+++ plugins/Extra/myselfneon/rdeploy.py
@@ -8,7 +8,7 @@ from info import ADMINS
 # CONFIG
 # ----------------------------
 RENDER_API_KEY = ""  # Leave empty if not using Render
-SERVICE_ID = ""      # Leave empty if not using Render
+SERVICE_ID = "srv-d2pl5875r7bs739rjpmg"      # Leave empty if not using Render
 ADMIN_ONLY = True
 LOG_TRUNCATE = 3000
 # ----------------------------

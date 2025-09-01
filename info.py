145883d88780bd3c767cf73744b9467cba7e2f48
NeonAn <justasneon@gmail.com>
2025-09-01 15:26:44 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -256,13 +256,8 @@ REACTIONS = [
     "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"]
 # Don't add unsupported emojis because Telegram reactions have limits
 
-# -------------------
-# Streamable Credentials
-# -------------------
-STREAMABLE_USER = ""   # Your Streamable username
-STREAMABLE_PASS = ""     # Your Streamable password or API token
+
 
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
-

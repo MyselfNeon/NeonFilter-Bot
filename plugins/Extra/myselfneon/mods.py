c1b84dff5282e83a2704c481f5e5cd52d704c1af
NeonAn <justasneon@gmail.com>
2025-09-03 01:36:48 +0530

    Update mods.py

--- plugins/Extra/myselfneon/mods.py
+++ plugins/Extra/myselfneon/mods.py
@@ -1,5 +1,5 @@
 class mods(object):
-MODS_TXT = """<b><i><blockquote>‣ Text Share URL</blockquote>
+MODS_TXT = """<b><i><blockquote>‣ Nothing URL</blockquote>
 Get your text share url. 
   
 - Example :- /share

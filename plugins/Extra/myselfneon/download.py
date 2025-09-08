cfe5e187cc6eafddb408c92686f3cb8a91412485
NeonAn <justasneon@gmail.com>
2025-09-08 12:03:14 +0530

    Update download.py

--- plugins/Extra/myselfneon/download.py
+++ plugins/Extra/myselfneon/download.py
@@ -317,11 +317,11 @@ def make_task_text(task: dict) -> str:
 
     text = (
         f"**{bar}**\n"
-        f"**__Sᴛᴀᴛᴜs: {status}__**\n"
-        f"**__Fɪʟᴇ: {fname}__**\n"
-        f"**__Pʀᴏᴄᴇssᴇᴅ: {human_readable(done)} / {human_readable(total)}__**\n"
-        f"**__Sᴘᴇᴇᴅ: {speed_str} | ETA: {sec_to_hms(eta)} | Elapsed: {sec_to_hms(elapsed)}__**\n\n"
-        f"**__Cᴀɴᴄᴇʟ:** /cancel_{task['id']}__\n"
+        f"**⚡ __Sᴛᴀᴛᴜs: {status}__**\n"
+        f"**🎬 __Fɪʟᴇ: {fname}__**\n"
+        f"**📦 __Pʀᴏᴄᴇssᴇᴅ: {human_readable(done)} / {human_readable(total)}__**\n"
+        f"**🚀 __Sᴘᴇᴇᴅ: {speed_str} | ETA: {sec_to_hms(eta)} | Elapsed: {sec_to_hms(elapsed)}__**\n\n"
+        f"**❌ __Cᴀɴᴄᴇʟ:** /cancel_{task['id']}__\n"
     )
     return text
 

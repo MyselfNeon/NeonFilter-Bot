8cf80d72ac4438c98d1a836b877e6a681d19ce59
NeonAn <justasneon@gmail.com>
2025-09-09 07:09:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -692,7 +692,7 @@ Also your given tutorial will be Added inside Your specified Group...</i></b>"""
 """
 
     STREAM_TXT = """<b><i><blockquote>‣ Get Stream And Download Link 📥</blockquote>
-Get Streamable and Downloadable link of any file by using /stream</i></b>"""
+Get Streamable and Downloadable link of any file by using /stream \n\n🆘 __Doesn’t work if the Bot is Deployed on Servers other than Heroku, and the URL values aren’t Set in the Repo.</i></b>"""
 
     MODS_TXT = """<b><i><blockquote>‣ Text Share URL</blockquote>
 Get your text share url. 

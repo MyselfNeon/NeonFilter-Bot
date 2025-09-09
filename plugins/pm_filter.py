672a69cd9f717514358c5b2358f5f747d0c8d611
NeonAn <justasneon@gmail.com>
2025-09-09 08:13:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1953,7 +1953,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴘ 🔊', url=GRP_LNK),
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🙏', url="https://t.me/ZeroFilterBot?start=Repolink")
+            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 🙏', url="https://myselfneon.github.io/my-links/")
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ 🏠', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ ❌', callback_data='close_data')
@@ -3288,13 +3288,3 @@ async def global_filters(client, message, text=False):
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
-
-
-
-
-
-
-
-
-
-

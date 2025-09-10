258b9ecc003ff632549bac7b003ce0398827c916
NeonAn <justasneon@gmail.com>
2025-09-10 23:54:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -162,8 +162,8 @@ Tᴏᴛᴀʟ Mᴇᴍʙᴇʀs = <code>{}</code>
 Aᴅᴅᴇᴅ Bʏ - {}"""
 
     LOG_TEXT_P = """**__#NewUser Started Bot 👤__**
-**🈂️ __UserID - <code>{}</code>__**
-**🆔 __UserName - {}__**"""
+\n\n**🈂️ __UsᴇʀNᴀᴍᴇ - <code>{}</code>__**
+**🆔 __UsᴇʀID - {}__**"""
 
     ALRT_TXT = """<b><i>Hello {}
 This is not your Movie/Series request.

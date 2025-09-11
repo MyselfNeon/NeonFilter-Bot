8ce4e22781bda2c22f3a153046d524f887fd9796
NeonAn <justasneon@gmail.com>
2025-09-11 21:33:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -156,14 +156,14 @@ OTHER DB :-
 ★ Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{} MB</code>
 ★ Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{} MB</code></i></b>"""
     
-    LOG_TEXT_G = """**✨ __#NewGroup__**
-**🏷️ __Gʀᴏᴜᴘ = {}__** \n**🆔 __Group ID = (<code>{}</code>)__**
+    LOG_TEXT_G = """**#NewGroup**
+\n**🏷️ __Gʀᴏᴜᴘ = {}__** \n**🆔 __Group ID = (<code>{}</code>)__**
 **👥 __Tᴏᴛᴀʟ Mᴇᴍʙᴇʀs = <code>{}</code>__**
 **➕ __Aᴅᴅᴇᴅ Bʏ - {}__**"""
 
-    LOG_TEXT_P = """**✨ __#NewUser__**
-**🆔 __UserID - <code>{}</code>__**
-**👤 __UserName - {}__**"""
+    LOG_TEXT_P = """**#NewUser**
+\n**🆔 __UserID - <code>{}</code>__**
+**👤 __User - {}__**"""
 
     ALRT_TXT = """<b><i>Hello {}
 This is not your Movie/Series request.

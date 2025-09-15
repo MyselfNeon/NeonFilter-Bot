16352b76a531b407f9a884e9b821a95c6d617116
NeonAn <justasneon@gmail.com>
2025-09-15 10:06:45 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -34,7 +34,7 @@ async def sng(bot, message):
             text=rpl,
             reply_to_message_id=message.id,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Aᴡᴇsᴏᴍᴇ Dᴇᴠᴇʟᴏᴘᴇʀ", url="tg://user?id=841851780")]]
+                [[InlineKeyboardButton("Aᴡᴇsᴏᴍᴇ Dᴇᴠᴇʟᴏᴘᴇʀ", url="https://myselfneon.github.io/neon/")]]
             ),
             disable_web_page_preview=True
         )
@@ -43,7 +43,7 @@ async def sng(bot, message):
             f"**__I Can't Find A Song With `{song}` 🚫__**",
             quote=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Aᴡᴇsᴏᴍᴇ Dᴇᴠᴇʟᴏᴘᴇʀ 😎", url="https://t.me/MyselfNeon")]]
+                [[InlineKeyboardButton("Aᴡᴇsᴏᴍᴇ Dᴇᴠᴇʟᴏᴘᴇʀ 😎", url="https://myselfneon.github.io/neon/")]]
             )
         )
 

c3adab271c1a05bffc7cd5f9c934e7dc84c7aff7
NeonAn <justasneon@gmail.com>
2025-09-24 00:44:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,7 +72,7 @@ async def start(client, message):
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('Cʀᴇᴀᴛᴇ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgIAAxkBAAIytGjS6CGkbAhRRWRpi22BQqDlRI9mAAJEGQACCOHZSVKp6_XqghKoHgQ") 
+        m=await message.reply_sticker("CAACAgIAAxkBAAIy12jS8TPJFHtoQh84Is8EsU4exixZAAKOFQACJU3BSY8WTX7r0TbzHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -213,7 +213,7 @@ async def start(client, message):
             if CLONE_MODE == True:
                 buttons.append([InlineKeyboardButton('😎 Cʀᴇᴀᴛᴇ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 😎', callback_data='clone')])
             reply_markup = InlineKeyboardMarkup(buttons)
-            m=await message.reply_sticker("CAACAgIAAxkBAAIytGjS6CGkbAhRRWRpi22BQqDlRI9mAAJEGQACCOHZSVKp6_XqghKoHgQ") 
+            m=await message.reply_sticker("CAACAgIAAxkBAAIy12jS8TPJFHtoQh84Is8EsU4exixZAAKOFQACJU3BSY8WTX7r0TbzHgQ") 
             await asyncio.sleep(1)
             await m.delete()
             await message.reply_photo(
@@ -1406,6 +1406,7 @@ async def purge_requests(client, message):
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 

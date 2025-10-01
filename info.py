6e37901aa6ab35174c2549147428fe77dcc99740
NeonAn <justasneon@gmail.com>
2025-10-02 00:14:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -140,9 +140,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'AnimeZerox') # Public 
 # ============================================================
 # 🔹 LINKS
 # ============================================================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+o1s-8MppL2syYTI9')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+wSKVngQbNtkyMTE1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/neonfiles')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Talk2NeonBot') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/neonfileschat') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MyselfNeon')
 
 
@@ -261,6 +261,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 

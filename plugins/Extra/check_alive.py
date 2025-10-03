94de6cbfec5c3b5e859ae1a5fd0c3dec280930cd
NeonAn <justasneon@gmail.com>
2025-10-03 13:36:44 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -22,7 +22,7 @@ async def check_alive(_, message):
         "🍀 Luck : Overflowing\n"
         "❤️‍🔥 Spirit : Unbreakable\n\n"
         "✌️ I'm Alive and Ready to Vibe\n"
-        "🪬 Summon Me With /start Command__**"
+        "🪬 Summon With /start Command !!__**"
     )
     await message.reply_text(alive_text)
 

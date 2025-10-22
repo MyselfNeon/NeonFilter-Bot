90b917ac820b72804b1dee56b0091129ea12609a
NeonAn <justasneon@gmail.com>
2025-10-22 20:51:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch
 # 🔹 DATABASE
 # ============================================================
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MyselfNeon")
+DATABASE_NAME = environ.get('DATABASE_NAME', "NeonFilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'neoncollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False))
@@ -263,6 +263,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 

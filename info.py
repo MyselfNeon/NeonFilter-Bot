6416c278da7d610b5fa81f1e6d099095b506ede4
NeonAnurag <justasneon@gmail.com>
2026-01-10 21:27:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -114,9 +114,9 @@ PAYMENT_TEXT = environ.get(
 
 # --- CLONE SETTINGS ---
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
-PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'AnimeZerox') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
+PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'NeonCodes') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # --- LINKS --- 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+o1s-8MppL2syYTI9')
@@ -220,6 +220,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 

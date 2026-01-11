0e170989e20ecfc255f49c81dd62972476e1a293
NeonAnurag <justasneon@gmail.com>
2026-01-11 11:56:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -307,20 +307,13 @@ Generates direct public links for your media files using external hosting servic
 02 - Catbox.moe</b>
   
 <blockquote><b>‣ Usage 📄</b></blockquote>
-<blockquote expandable>
-Send /telegraph in the bot's private chat.
-Select your preferred hosting site (ImgBB or Catbox) using the buttons.
-Send the file (Photo, Video, Audio, or Document) within 60 secs.
-The bot will download, upload, and provide a sharable link.
-</blockquote>
+<blockquote expandable>Send /telegraph in the bot's private chat. ​Select your preferred hosting site (ImgBB or Catbox) using the buttons. ​Send the file (Photo, Video, Audio, or Document) within 60 secs. ​The bot will download, upload, and provide a sharable link.</blockquote>
 
 <blockquote><b>‣ Key Features ⚙️</b></blockquote>
-<blockquote>
-• Photos, Videos, Audio, and Docs.
+<blockquote>​• Photos, Videos, Audio, and Docs.
 • Size Limit: Max 200MB per file.
 • Auto-cancels if exceeds 60 seconds.
-• Use /tcancel to stop manually.
-</blockquote>
+• Use /tcancel to stop manually.</blockquote>
 
 <blockquote><b>‣ Important Notes ⚠️</blockquote>
 <blockquote>Private Only: This command is currently set to work only in Private Messages (PM), not groups.</blockquote></b></i>"""
